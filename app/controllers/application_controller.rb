1e64f16fa1046ac742beb0f74e171c53aefd2229
aomalley574 <amanda.omalley@kellogg.northwestern.edu>
2020-06-02 03:50:25 +0000

    established basic structure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+
+  def homepage
+    render({ :template => "nuvaapp/homepage.html.erb"})
+
+  end
+
+
+  def tracked
+    
+    render({ :template => "nuvaapp/tracked.html.erb"})
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+get("/", { :controller => "application", :action => "homepage" })
 
+get("/tracked", { :controller => "application", :action => "tracked" })
 
 end
